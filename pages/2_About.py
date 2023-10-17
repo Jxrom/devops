c76f558501c25432ee268d5d7d6cdb2772d28616
Jxrom <68335788+Jxrom@users.noreply.github.com>
2023-10-17 21:51:48 +0800

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -4,6 +4,43 @@ import numpy as np
 import cv2
 from PIL import Image, ImageOps
 
+st.set_page_config(page_title="Classification Page", page_icon="🔎")
+
+@st.cache
+def load_model():
+    model = tf.keras.models.load_model("assets/car_bike_classifier.h5", compile=False)
+    model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['acc'])
+    return model
+
+def import_and_predict(image_data, model):
+    size = (75, 75)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    img = np.asarray(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_reshape = gray[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+st.write("""
+# Car-Bike Detection
+""")
+
+uploaded_images = st.file_uploader("Choose up to 5 Car or Bike photos from your computer", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+
+if not uploaded_images:
+    st.text("Please upload image files")
+else:
+    model = load_model()
+    class_names = ["Bike", "Car"]
+    num_images = min(len(uploaded_images), 5)
+
+    for i in range(num_images):
+        image = Image.open(uploaded_images[i])
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        string = "OUTPUT : " + class_names[np.argmax(prediction)]
+        st.success(string)
+
 # Discussion of best architectures for classification in deep learning
 st.write("""
 ## Best Architectures for Classification in Deep Learning
@@ -37,7 +74,7 @@ architectures = {
     ]
 }
 
-# Create a DataFrame and display it with adjusted column widths
+# Create a DataFrame and display it as a table
 import pandas as pd
 df = pd.DataFrame(architectures)
-st.dataframe(df, width=700)
+st.table(df)
